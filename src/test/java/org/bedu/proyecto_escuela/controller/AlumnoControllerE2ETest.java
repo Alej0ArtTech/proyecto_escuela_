25ea3a67aa2bc283c74fbb0d704e71a4337bf545
Alejodev <honki.ponki.dance@gmail.com>
2024-02-01 19:31:00 -0600

    Actualizando la primera prueba E2E

--- src/test/java/org/bedu/proyecto_escuela/controller/AlumnoControllerE2ETest.java
+++ src/test/java/org/bedu/proyecto_escuela/controller/AlumnoControllerE2ETest.java
@@ -1,6 +1,7 @@
 package org.bedu.proyecto_escuela.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bedu.proyecto_escuela.mapper.AlumnoMapper;
 import org.bedu.proyecto_escuela.repository.AlumnoRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,12 +14,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//Anotación para configurar la BD para las pruebas
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 //Anotacion para trabajar con mock en ambiente MVC:
 @AutoConfigureMockMvc
-//
+//Sirven para configurar Sping para test
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 
@@ -30,15 +36,29 @@ class AlumnoControllerE2ETest {
     @Autowired
     private AlumnoRepository repository;
 
-    private AlumnoMapper mapper = new AlumnoMapper();
+    private ObjectMapper mapper = new ObjectMapper();
 
+    //Before Each sirve para establecer parametros antes de ejecutar las pruebas
     @BeforeEach
     public void setup() {
         repository.deleteAll();
     }
 
     @Test
-    @DisplayName("GET /A")
+    @DisplayName("GET /Alumnos deberia regresar una lista vacia")
+    void emptyListTest() throws Exception {
+        //Realizar una petición Get a través de un Mock, esperando 200
+        MvcResult result = mockMvc.perform(get("/alumno"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        assertEquals("[]", content);
+
+    }
+
+    //
+
 
 
 
